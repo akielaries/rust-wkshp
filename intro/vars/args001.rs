e3596bbe3b40609fe271fac1d042232b409918e8
montaquila <akiel@akiel.org>
2022-10-01 18:15:52 -0700

    more practice with args

--- intro/vars/args001.rs
+++ intro/vars/args001.rs
@@ -2,7 +2,13 @@ fn print_sum(a: i8, b: i8) {
     println!("sum is: {}", a + b);
 }
 
+fn plus_uno(a: i32) {
+    println!("append sum: {}", a + 1);
+}
+
 fn main() {
     print_sum(3, 4);
+    
+    plus_uno(7);
 }
 
